0fd367e80b5c426feed5ae2dec35393ca261dbd6
vivoanandh <109234888+Anandhakumar123456@users.noreply.github.com>
2023-07-26 17:25:50 +0530

    practice_problems_02copy.py file deleted

--- Practice_problems/practice_problem_02.py
+++ Practice_problems/practice_problem_02.py
@@ -118,18 +118,273 @@ print(mylist) #10 is missing.
 
 # 7) Dictionary Operations:
 
-mydict={'Anandhakumar': '12345', 'Babu': 'babu', 'Saran': 'saran123', 'Ganesh': 'ganesh123', 'Ram': 'ram123', 'Ammu': 'ammu123', 'Bala': 'bala123', 'Suresh': 'suresh123', 'Sam': 'sam123', 'Tom': 'tom123', 'Gokul': 'gokul123'}
+def pass_check():
 
-print(mydict)
+    mydict={'Anandhakumar': '12345', 'Babu': 'babu', 'Saran': 'saran123', 'Ganesh': 'ganesh123', 'Ram': 'ram123', 'Ammu': 'ammu123', 'Bala': 'bala123', 'Suresh': 'suresh123', 'Sam': 'sam123', 'Tom': 'tom123', 'Gokul': 'gokul123'}
+
+#print(mydict)
+
+    for username,password in mydict.items():
+        username=input("Enter the Username:")
+        password=input("Enter the Password:")
+        if username not in mydict.keys():  
+            print("The username is not valid for the system.")
+        elif username in mydict.keys() and password not in mydict.values():
+            print("The password is invalid.") 
+        else:
+            print("You are Logged in the system.")  
+            break
 
 
-for username,password in mydict.items():
-    username=input("Enter the Username:")
-    password=input("Enter the Password:")
-    if username not in mydict.keys():  
-        print("The username is not valid for the system.")
-    elif username in mydict.keys() and password not in mydict.values():
-        print("The password is invalid.") 
+#pass_check()
+
+#8) Rotates the elements in the list.
+
+lst=[2,6,3,8,4]
+
+
+def rotate_list(lst):
+    if not lst:  # not means empty list
+        return lst
+    last_item=lst[-1]
+    for i in range(len(lst)-1,0,-1):
+        lst[i]=lst[i-1]   #lst[4]=lst[4-1]
+    lst[0]=last_item
+    return lst    
+
+rotated_list=rotate_list(lst)
+#print(rotated_list)
+
+
+
+#9) Count donuts:
+
+def donuts(count):
+    if count>=10:
+        return "Number of donuts  : 'many' "
     else:
-        print("You are Logged in the system.")  
-        break
+        return "Number of donuts  :{}".format(count)
+    
+
+#print(donuts(1)) 
+
+#10) Return 2 character of string:
+
+
+def first_and_last2char(s):
+    if len(s)==2:
+        return(s)
+    else:    
+        return(s[0:2]+s[-2:])
+
+#print(first_and_last2char("sp"))
+
+
+
+#11) Check the same strings first and last characters:
+
+strings=['deed','hello','world','mam','pop','sos','laptop','did']
+def same_end_strings():
+    count=0
+    for i in strings:
+        if len(i)>=2 and i[0]==i[-1]:
+            count+=1  # if above the conditions are true the count variable incremented by one. 
+    return(count)        
+
+#x=same_end_strings()
+#print(x)
+
+
+#12) Multiples of 3 or 5 below 1000:
+
+def sum_multiple_3_5(limit):
+    sum_of_num=0
+    for i in range(limit):
+        if i%3==0 or i%5==0:
+            sum_of_num+=i
+    return sum_of_num
+
+#print(sum_multiple_3_5(100))
+
+
+#13) Fibonacci Series:
+
+
+def fibonacci_sum_even(limit):
+    prev_num = 1
+    current_num = 2
+    even_sum = 0
+
+    while current_num <= limit:
+        if current_num % 2 == 0:  
+            even_sum += current_num
+
+        next_num = prev_num + current_num
+        prev_num = current_num
+        current_num = next_num
+
+    return even_sum
+
+#print(fibonacci_sum_even(4000000))
+
+
+
+#14) Remove duplicates in array:
+
+array=[1,2,3,4,5,6,5,4,3,2,1]
+
+def remove_duplicates(array):
+    return list(set(array)) #Bcz set is not allow duplicate items 
+
+#print(remove_duplicates(array))
+
+
+#15) Random code generator :
+
+import random
+
+def random_with_no_range(min_value,max_value):
+    return random.randint(min_value,max_value)
+
+#print(random_with_no_range(1,90))
+
+#16) Prime Numbers:
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def sum_of_primes_below(limit):
+    prime_sum = 0
+    for number in range(2, limit):
+        if is_prime(number):
+            prime_sum += number
+    return prime_sum
+
+
+#17) Not present in the array:
+
+array1=[1,2,3,4,5]
+array2=[2,3,1,0,5]
+
+def miss_number(arr1,arr2):
+    for i in arr1:
+        if i not in arr2:
+            return f"The missing Number is :{i}"
+
+#print(miss_number(array1,array2))
+
+
+#18) Sum of squres:
+
+def sum_of_squares(n):
+    return sum(i**2 for i in range(1, n+1))  # 100+81+64+49+36+25+16+9+4+1  =385
+
+def square_of_sum(n): # 10+9+8+7+6+5+4+3+2+1 =55 **2 = 3025
+    return sum(range(1, n+1))**2  #sum( range(1, 11 ))**2
+
+
+limit = 100
+
+# Calculate the sum of the squares of the first one hundred natural numbers
+
+sum_of_squares_result = sum_of_squares(limit)
+
+# Calculate the square of the sum of the first one hundred natural numbers
+
+square_of_sum_result = square_of_sum(limit)
+
+# Find the difference
+
+difference = square_of_sum_result - sum_of_squares_result
+
+#print(f"The difference is: {difference}")
+
+#19) Area and perimeter of the circle:
+
+def area_and_perimeter():
+    pi=3.14
+    r=3
+    print("Area of the circle is :"+pi*r*r)
+    print("Perimeter of the circle is :"+2*pi*r)
+
+#20) Greetings:
+
+greeting = "Hello Google!"
+
+number_of_characters=len(greeting)
+
+repeated_greeting=number_of_characters*greeting
+print("Repeated greetings: ",repeated_greeting)
+
+
+#21) reads an integer N (1 < N < 1000):
+
+def generate_output(N):
+    for i in range(1,N+1):
+        print(f"{i} {i*2} {i**3}")
+
+N=int(input("Enter the limit: "))
+
+if N<1000:
+    print(generate_output(N))
+else:
+    print("Invalid Number. N Should be 1 to 999.")    
+
+
+#22) Password validation:
+"""import re
+def password_validation(password):
+    if len(password)<=6 :
+        print("Password must be above 6-8 characters.")
+    else:
+        re.match("r^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$",password)
+        
+password=input("Enter the password for validation:")
+
+passwords_sample=["abc123", "Passw0rd", "qwerty", "Abc1234", "myp@ss"]
+
+for i in passwords_sample:
+    if password_validation(i):
+        print(f"Password {password} is valid.")
+    else:
+        print(f"Password {password} is invalid.")   """ 
+
+#23) Read two numbers M and N indefinitel factorial :
+
+
+
+#24) Calculate salary:
+
+
+def calculate_increase_salary(salary):
+    if salary <= 400.00:
+        increase_percentage = 15
+    elif salary <= 800.00:
+        increase_percentage = 12
+    elif salary <= 1200.00:
+        increase_percentage = 10
+    elif salary <= 2000.00:
+        increase_percentage = 7
+    else:
+        increase_percentage = 4
+    
+    increase_amount = salary * (increase_percentage / 100)
+    new_salary = salary + increase_amount
+    
+    return new_salary, increase_amount, increase_percentage
+
+def main():
+    salary = float(input("Enter the salary:").strip())
+    new_salary, increase_amount, increase_percentage = calculate_increase_salary(salary)
+    
+    print("Novo salario: {:.2f}".format(new_salary))
+    print("Reajuste ganho: {:.2f}".format(increase_amount))
+    print("Em percentual: {} %".format(increase_percentage))
+
+
+main()
